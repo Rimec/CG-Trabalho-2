e61d1c769beec2a1496fc8933c1c288492b4c6e1
Renan Rodrigues Moura <93983290+Rimec@users.noreply.github.com>
2023-04-08 18:33:40 -0300

    feat: VoxelTerrain

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int xSize = 20;
+    public int zSize = 20;
+
+    public float height;
+    public float entropy;
+    public Gradient gradient;
+    public GameObject box;
     void Start()
     {
-        
+        CreateMesh();
     }
-
-    // Update is called once per frame
-    void Update()
+    void CreateMesh()
     {
-        
+        for (var z = 0; z < zSize; z++)
+        {
+            for (var x = 0; x < xSize; x++)
+            {
+                float noiseX = (x / (float)xSize) * entropy;
+                float noiseZ = (z / (float)zSize) * entropy;
+                float y = Mathf.PerlinNoise(noiseX, noiseZ) * height;
+                Vector3 pos = new Vector3(x, Mathf.Round(y), z);
+                GameObject obj = Instantiate(box, pos, Quaternion.identity);
+                Material m = obj.GetComponent<MeshRenderer>().material;
+                m.color = gradient.Evaluate(y / this.height);
+            }
+        }
     }
 }
